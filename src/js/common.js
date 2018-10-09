540bf5088795dde7105cd1f008a951679c6a308c
unknown <https://github.com/VitaliiThir>
2018-10-09 18:03:11 +0400

    last

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,5 @@
 $(function () {
+
 // Slider
     $("#owl-about_us").owlCarousel({
         nav: true,
@@ -31,4 +32,35 @@ $(function () {
     var arrowNext = $('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="8" height="14" viewBox="0 0 8 14"><defs><path id="xzwva" d="M1347 1921.84c0 .12.05.23.14.32l.68.7c.09.08.21.14.32.14.11 0 .23-.06.32-.14l6.4-6.54a.49.49 0 0 0 .14-.32.49.49 0 0 0-.14-.32l-6.4-6.54a.47.47 0 0 0-.32-.14.47.47 0 0 0-.32.14l-.68.7a.49.49 0 0 0-.14.32c0 .12.05.24.14.33l5.4 5.51-5.4 5.51a.49.49 0 0 0-.14.33z"/></defs><g><g transform="translate(-1347 -1909)"><use fill="#ddd" xlink:href="#xzwva"/></g></g></svg>');
     $('.theme .owl-prev').append($(arrowPrev));
     $('.theme .owl-next').append($(arrowNext));
+
+    // Latest works nav
+    $('.latest-nav li').on('click', function () {
+        $(this).each(function () {
+            if ($('.latest-nav li').hasClass('active')) {
+                $('.latest-nav li').removeClass('active').not(this);
+            }
+        })
+    });
+
+
+    // Mix It Up
+    $('#mix-row').mixItUp();
+
+    // Reviews Slider
+    $("#reviews-slider").owlCarousel({
+        nav: true,
+        navSpeed: 1500,
+        autoplaySpeed: 3000,
+        autoplayTimeout: 5000,
+        items: 1,
+        margin: 30,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        pagination: false,
+        navText: ["", ""],
+        theme: "theme-2"
+    });
+
 });
+
